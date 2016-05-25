17b22e2ff1e9011544d2dfaa6bfa244de3d45a79
Peter Yoon <peyo.hd@gmail.com>
2016-05-26 00:59:39 +0900

    link to libmesa_nir, libmesa_compiler

--- Android.mk
+++ Android.mk
@@ -165,7 +165,9 @@ LOCAL_STATIC_LIBRARIES += \
 	libmesa_gallium \
 	libmesa_glsl \
 	libmesa_glsl_utils \
-	libmesa_util
+        libmesa_nir \
+	libmesa_util \
+        libmesa_compiler
 LOCAL_SHARED_LIBRARIES += libdl
 endif # DRM_USES_PIPE
 include $(BUILD_SHARED_LIBRARY)
